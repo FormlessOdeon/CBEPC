e0369808a112c1a680cc921894e7d90069a7d981
FormlessOdeon <innsmouthlibrarian@gmail.com>
2017-04-12 15:13:41 -0400

    Added core function loop.

--- Main Module.py
+++ Main Module.py
@@ -6,11 +6,11 @@ import numpy as np
 
 #Initialize primary variables.
 #ComicList = input('Enter path of file')
-ComicList = "C:\\Users\sam.APM\Desktop\PyTest\Testspreadsheet.xlsx"
+ComicList = "C:\ProjectRepo\CBEPC\Testspreadsheet.xlsx"
 PanComicList = pd.read_excel(ComicList)
 IssueColumn = PanComicList['Issues']
 TitleColumn = PanComicList['Title']
-TCount = 0
+TCount = -1
 IList = []
 TList = []
 TNI = []
@@ -19,22 +19,28 @@ TNI = []
 for TitleCell in TitleColumn:
     TList.append(str(TitleCell))
 
-#Create array of issue numbers. Each element of the list is a list. The inner list's elements are the specific issue numbers seperated by commas
+# Create array of issue numbers. Each element of the list is a list. The inner list's elements are the specific issue
+# numbers seperated by commas
 for IssueCell in IssueColumn:
     Issues = (str(IssueCell).split(','))
     IList.append(Issues)
-    #Create a list where each element is a title and issue number. There should be an element for every single issue where the title is matched with every issue number in it's corresponding list.
-    #This is presently the #1 challenge with this app
-    #for Issue in Issues:
-        #print(TList[TCount])
-        #TNI.append(ListAdd)
-        #TCount = TCount + 1
+
+# Create a list where each element is a title and issue number. There should be an element for every single issue
+# where the title is matched with every issue number in it's corresponding list. This is presently the #1 challenge
+# with this app
+for T in TList:
+    TCount += 1
+    for I in IList[TCount]:
+        TNI.append(T + I)
+
+
+
 
 
 
 #print(TList)
 #print(IList)
-#print(TNI)
+print(TNI)
 #print(PanComicList)
 
 
